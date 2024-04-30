38e79ab32b93789723d3233fe8d3cdb8115f58c5
Ankush Agarwal <161926801+AnkushAgarw@users.noreply.github.com>
2024-04-30 20:48:11 +0530

    Add Function to display number buttons being clicked

--- script.js
+++ script.js
@@ -2,8 +2,21 @@ let firstValue
 let secondValue
 let operator
 
+let display = document.querySelector(".display");
+let buttons = document.querySelector(".buttons")
 
+buttons.addEventListener("click", (e) => {
+    targetID = e.target.id;
+    if (e.target.className === "number") {
+        display.textContent += targetID
+    }
+    if (e.target.className === "operation") {
 
+    }
+    if (targetID === "clear") {
+        display.textContent = ""
+    }
+})
 
 function add(a, b) {
     return a + b;
@@ -19,4 +32,21 @@ function multiply(a, b) {
 
 function divide(a, b) {
     return a / b;
+}
+
+function operate(operator, numA, numB) {
+    switch (operator) {
+        case "add":
+            add(numA, numB);
+            break;
+        case "subtract":
+            subtract(numA, numB);
+            break;
+        case "multiply":
+            multiply(numA, numB);
+            break;
+        case "divide":
+            divide(numA, numB);
+            break;
+    }
 }
